b35aaa03d184fdd42c937cb2ef6c20c9f382cbe2
almus <Almas>
2019-06-14 12:53:53 +0600

    handling user inputs

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,19 +1,60 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  String descriptionValue = '';
+  double priceValue = 0.0;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-        child: RaisedButton(
-      child: Text('Save'),
-      onPressed: () {
-        showModalBottomSheet(
-            context: context,
-            builder: (context) {
-              return Center(child: Text('This is a modal'),);
-            });
-      },
-    ));
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Name of the product'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            maxLines: 6,
+            decoration: InputDecoration(
+              labelText: 'Describe the product'
+            ),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Price',
+            ),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+
+        MaterialButton(
+          onPressed: () {},
+          child: Text('Save'),
+        )
+        ],
+      ),
+    );
   }
 }
